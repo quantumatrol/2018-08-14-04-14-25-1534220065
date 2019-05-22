52b31e2f93c1eb7227c565144587d0321a197c56
WangHuancheng <wangkang980930@outlook.com>
2019-05-23 00:39:39 +0800

    update

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
-module.exports = function main() {
-    console.log("Debug Info");
+module.exports = function main(number) {
+    var lcdFirstLine =  ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
+    var lcdSecondLine = ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'];
+    var lcdThirdLine =  ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'];
+
+    console.log("Debugger");
     return 'Hello World!';
 };
